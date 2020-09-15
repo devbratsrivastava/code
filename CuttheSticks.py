c44c8b764c1d7b6ae6280ee6f5ed5c13b54b7b9f
Devbrat Srivastava <devbrat0srivastava@gmail.com>
2020-09-15 15:34:21 -0700

    Create CuttheSticks.py

---
+++ CuttheSticks.py
@@ -0,0 +1,15 @@
+n=int(input())
+list1=[]
+list1=((input())).split()
+list2=[]
+for i in list1:
+    list2.append(int(i))
+while(len(list2)>0):
+    print(len(list2))
+    x=min(list2)
+    for i in range(len(list2)):
+        list2[i]-=x
+    while(list2.count(0)):
+        list2.remove(0)
+    
+    
