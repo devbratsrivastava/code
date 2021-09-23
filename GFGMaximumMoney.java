d1ee27b29f30dd2252f5c390be5585a5eec0d241
Devbrat Srivastava <devbrat0srivastava@gmail.com>
2021-09-24 01:07:30 +0530

    Create GFGMaximumMoney.java

---
+++ GFGMaximumMoney.java
@@ -0,0 +1,9 @@
+class Solution {
+    static int maximizeMoney(int N , int K) {
+        // code here
+        if(N%2==0)
+        return (N/2)*K;
+        else
+        return ((N/2)+1)*K;
+    }
+};
