756b769d29b03ac925330c6d40a80799bce8001b
Devbrat Srivastava <devbrat0srivastava@gmail.com>
2022-08-11 19:59:49 +0530

    Update GFG_RearrangeArrayAlternatively.java
    https://practice.geeksforgeeks.org/problems/-rearrange-array-alternately-1587115620/1

--- GFG_RearrangeArrayAlternatively.java
+++ GFG_RearrangeArrayAlternatively.java
@@ -6,25 +6,17 @@ class Solution{
     public static void rearrange(long arr[], int n){
         
         // Your code here
-        long t1=0;
-        //int j=n-1;
-        //int i=0;
-        int k=1;
-        for(int i=n-1;i>=n/2;i--)
+        int maxind=n-1,minind=0;
+        long maxele=arr[n-1]+1;
+        for(int i=0;i<n;i++)
         {
-            t1=arr[n-1];
-            //int k=1;
-            int j=n-1;
-            for(;j>=k;j--)
-            {
-                arr[j]=arr[j-1];
-            }
-            arr[j]=t1;
-            k+=2;
-            //for(j=0;j<n;j++)
-            //    System.out.print(arr[j]+" ");
-            //System.out.println();
+            if(i%2==0)
+            arr[i]+=(arr[maxind--]%maxele)*maxele;
+            else
+            arr[i]+=(arr[minind++]%maxele)*maxele;
         }
+        for(int i=0;i<n;i++)
+        arr[i]/=maxele;
     }
     
 }
